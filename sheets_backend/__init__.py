e3664fae536aa68e9c13a64315698ac08250d6f9
charles rymal <charlesrymal@gmail.com>
2017-04-30 06:03:32 -0700

    work on sheets_backend structure

--- sheets_backend/__init__.py
+++ sheets_backend/__init__.py
@@ -1,14 +1,35 @@
 
+class Storage(object):
+    """
+    An abstaction for reading and writing sheets to
+    and from long term storage
+    """
+    def read(sheet_id): pass
+    def write(sheet_id, b):
+        """
+        :param b: bytes
+        """
+        pass
+    pass
 
-
-
-
-
+class Server(object):
+    """
+    This class is an abstaction for sending and
+    receiving sheet data to and from a ServerProxy object.
+    The purpose of the Server is to keep
+    Sheet objects loaded in memory.
+    """
+    def __init__(self, storage):
+        """
+        :param storage: a Storage object
+        """
+        self.storage = storage
 
 class SheetProxy(object):
     """
-    This class creates an abstraction for accessing a
-    Sheet object through different backends.
+    This class is an abstraction for access to a
+    Sheet object through communication with a Server
+    object.
     """
     
 
