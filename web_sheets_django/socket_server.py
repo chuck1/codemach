0182a991a0035f4fcae8e653a556535c76cbe649
Charles Rymal <charles.rymal@nortek.com>
2017-05-11 14:03:22 -0700

    get port from secrets, change sheets file location

--- web_sheets_django/socket_server.py
+++ web_sheets_django/socket_server.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 import os
+import json
 import sys
 
 import sheets_backend.filesystem
 import sheets_backend.sockets
 
 def test():
-    folder = '/home/chuck/sheets'
-    port = 10001
+
+    secrets = json.loads(open("secrets.json", "r").read())
+
+    folder = "/etc/web_sheets"
+    port = secrets["port"]
 
     storage = sheets_backend.filesystem.Storage(folder)
     server = sheets_backend.sockets.Server(storage, port)
