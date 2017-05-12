3d7bf6c8058b5859b923ec0fdbbaf837d53cca88
Charles Rymal <charles.rymal@nortek.com>
2017-05-12 14:00:59 -0700

    update tests. add inter-sheet reference test

--- sheets/tests/set_script_pre.py
+++ sheets/tests/set_script_pre.py
@@ -4,15 +4,10 @@ import sheets
 def test():
     b = sheets.Book()
 
-    expected_output = 'Traceback (most recent call last):\n  File "/home/crymal/git/python_packages/web_sheets/sheets/script.py", line 46, in execute\n    exec(self.code, g)\n  File "<script>", line 1, in <module>\n  File "/home/crymal/git/python_packages/web_sheets/sheets/__init__.py", line 42, in builtin___import__\n    raise ImportError("module \'{}\' is not allowed".format(name_split[0]))\nImportError: module \'os\' is not allowed\n'
-
     b.set_script_pre('import os')
     b.do_all()
 
-    #print(repr(b.script_pre.output))
-
-    if b.script_pre.output != expected_output:
-        raise RuntimeError('output does not match expected')
+    assert(repr(b.script_pre.exec_exc) == "ImportError(\"module 'os' is not allowed\",)")
 
     b.set_script_pre("import math\n")
 
