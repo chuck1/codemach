fb86aacce65c4c9071a8f9ef1f77c4e7a0241ff2
charles rymal <charlesrymal@gmail.com>
2017-05-14 09:49:33 -0700

    work on install and upstart scripts

--- install.bash
+++ install.bash
@@ -1,3 +1,30 @@
+#!/bin/bash 
 
-cp daemon/web_sheets_storage.bash
+cd ..
+
+cd mysocket
+pip3 install -e .
+cd ..
+
+cd storage
+pip3 install -e .
+cd ..
+
+cd sheets
+pip3 install -e .
+cd ..
+
+cd sheets_backend
+pip3 install -e .
+cd ..
+
+cd web_sheets
+
+for f in daemon/*.conf; do
+	ln -sf `pwd`/$f /etc/init/$(basename "$f" .conf).conf
+done
+
+#ln -s daemon/web_sheets_storage.conf /etc/init/web_sheets_storage.conf
+#ln -s daemon/web_sheets_storage.conf /etc/init/web_sheets_storage.conf
+
 
