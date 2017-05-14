392a9cecde548cca46176cc4eb105438e5003712
charles rymal <charlesrymal@gmail.com>
2017-05-14 09:50:53 -0700

    make into python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(name='mysocket',
+        version='0.1',
+        description='convenient wrapper for python socket',
+        url='http://github.com/chuck1/mysocket',
+        author='Charles Rymal',
+        author_email='charlesrymal@gmail.com',
+        license='MIT',
+        packages=['mysocket'],
+        zip_safe=False,
+        )
+
