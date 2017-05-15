9a86ea5e66c19aceef9286b42e1ef48c527ab5d4
charles rymal <charlesrymal@gmail.com>
2017-05-15 13:23:13 -0700

    make independent of sheets

--- storage/filesystem/__init__.py
+++ storage/filesystem/__init__.py
@@ -1,11 +1,15 @@
 import os
 import pickle
+import logging
 
 import storage
 
+logger = logging.getLogger(__name__)
+
 class Storage(storage.Storage):
     
-    def __init__(self, folder):
+    def __init__(self, cls, folder):
+        self.cls = cls
         self.folder = folder
         self.objects = {}
 
@@ -23,24 +27,24 @@ class Storage(storage.Storage):
 
         return str(i)
 
-    def book_new(self):
-        b = sheets.Book()
+    def object_new(self):
+        b = self.cls()
         i = self.next_id()
         self.objects[i] = b
         self.write(i, b)
         return i, b
 
-    def get_book(self, object_id):
+    def get_object(self, object_id):
         if not object_id in self.objects:
-            self.objects[object_id] = self.read(book_id)
+            self.objects[object_id] = self.read(object_id)
         
         return self.objects[object_id]
 
-    def save_book(self, object_id):
+    def save_object(self, object_id):
         if not object_id in self.objects:
             raise RuntimeError('attempt to save sheet that is not loaded')
 
-        self.write(object_id, self.objects[book_id])
+        self.write(object_id, self.objects[object_id])
 
     def read(self, object_id):
         """
@@ -55,17 +59,17 @@ class Storage(storage.Storage):
         with open(filename, 'rb') as f:
             o = pickle.loads(f.read())
         
-        if not isinstance(o, sheets.Book):
+        if not isinstance(o, self.cls):
             raise TypeError()
 
         return o
 
-    def write(self, object_id, book):
+    def write(self, object_id, object):
         """
         :param object_id: sheet id
         :param sheet: sheets.Sheet object
         """
         filename = os.path.join(self.folder, str(object_id)+'.bin')
         with open(filename, 'wb') as f:
-            f.write(pickle.dumps(book))
+            f.write(pickle.dumps(object))
 
