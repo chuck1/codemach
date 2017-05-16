5f188e0a731aa9d53337bb1e00d9430ec549116c
Charles Rymal <charles.rymal@nortek.com>
2017-05-16 14:23:25 -0700

    make folder nav more robust

--- install.bash
+++ install.bash
@@ -1,30 +1,25 @@
 #!/bin/bash 
 
-cd ..
+root=`pwd`
 
-cd mysocket
+
+cd $root/mysocket
 pip3 install -e .
-cd ..
 
-cd storage
+cd $root/storage
 pip3 install -e .
-cd ..
 
-cd sheets
+cd $root/sheets
 pip3 install -e .
-cd ..
 
-cd sheets_backend
+cd $root/sheets_backend
 pip3 install -e .
-cd ..
 
-cd web_sheets
+cd $root
 
 for f in daemon/*.conf; do
 	ln -sf `pwd`/$f /etc/init/$(basename "$f" .conf).conf
 done
 
-#ln -s daemon/web_sheets_storage.conf /etc/init/web_sheets_storage.conf
-#ln -s daemon/web_sheets_storage.conf /etc/init/web_sheets_storage.conf
 
 
