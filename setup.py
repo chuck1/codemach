f31d2f8787883952aa2b081b1562a6cc1207bed3
Charles Rymal <charles.rymal@nortek.com>
2017-05-16 17:02:19 -0700

    trying to create proper setup.py for root

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup
 
-setup(name='sheets',
+setup(name='web_sheets',
         version='0.1',
         description='python spreadsheets',
         url='http://github.com/chuck1/sheets',
@@ -9,6 +9,7 @@ setup(name='sheets',
         author_email='charlesrymal@gmail.com',
         license='MIT',
         packages=['sheets'],
+        package_dir={'sheets':'sheets'},
         install_requires=[
             'fs',
             'numpy',
