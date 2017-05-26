c57d75dbf1bdc27a0f80d3b139ce42b6a9af3ecd
charles rymal <charlesrymal@gmail.com>
2017-05-26 13:33:23 -0700

    fix deployment setup

--- install_django.bash
+++ install_django.bash
@@ -1,14 +1,22 @@
 
+mkdir /var/log/web_sheets_django -p
+
+chown -R www-data:www-data /var/log/web_sheets_django
+
 
 root=`pwd`
 
-cp -f config/apache.conf /etc/apache2/apache2.conf
+cp -f config/web_sheets.conf /etc/apache2/sites-available
+a2ensite web_sheets.conf
+
 
 rsync -av web_sheets_django /var/www
 
-cd web_sheets_django
+#cd web_sheets_django
+#python3 manage.py collectstatic --noinput
+#cd $root
 
-python3 manage.py collectstatic --noinput
+chown -R www-data:www-data /var/www/web_sheets_django
 
-cd $root
+
 
--- web_sheets_django/web_sheets_django/settings/__init__.py
+++ web_sheets_django/web_sheets_django/settings/__init__.py
@@ -3,6 +3,6 @@ from web_sheets_django.settings.base import *
 
 DEBUG = False
 
-
+LOGGING['handlers']['file']['filename'] = '/var/log/web_sheets_django/debug.log'
 
 
