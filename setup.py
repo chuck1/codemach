01155db53458b3a2cb28ff14aea26c0db165a0bb
charles rymal <charlesrymal@gmail.com>
2017-07-01 11:52:46 -0700

    remove dependence on pkgtool

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
 import os
+import re
 from setuptools import setup
+import toml
 
-import pkgtool
+with open('Pytool') as f:
+    c = toml.loads(f.read())
 
-pkg = pkgtool.Package(os.getcwd())
+with open(os.path.join(c['name'], '__init__.py')) as f:
+    version = re.findall("^__version__ = '(.*)'", f.read())[0]
 
-setup(**pkg.setup_args())
+with open('requirements.txt') as f:
+    install_requires=[l.strip() for l in f.readlines()]
+    
+kwargs = {
+        'name': c['name'],
+        'version': version,
+        'description': c['description'],
+        'url': c['url'],
+        'author': c['author'],
+        'author_email': c['author_email'],
+        'license': c['license'],
+        'packages': c.get('packages', []),
+        'zip_safe': False,
+        'scripts': c.get('scripts',[]),
+        'package_data': c.get('package_data',{}),
+        'install_requires': install_requires,
+        'setup_requires': ['toml']
+        }
+
+setup(**kwargs)
+
+
 
